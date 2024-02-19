c9914a02d52bcca0dc09ebc380db8258a45746b5
Isabel Solana Santamaria <mariaisabelsolanasantamaria.gmail.com>
2024-02-19 20:17:09 +0000

    Initialize Project

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+      <>
+          {/* <Navbar />
+          <Social />
+          <Landing />
+          <ProjectList />
+          <About />
+          <Contact /> */}
+        </>
+      </div>
+     
+    </>
+  )
+}
+
+export default App
