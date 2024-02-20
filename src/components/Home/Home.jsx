43dc59bc6e4715b89f624f309e05c3dbbc39cc2d
Isabel Solana Santamaria <mariaisabelsolanasantamaria.gmail.com>
2024-02-20 16:17:53 +0000

    Home info and photo updated

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -8,18 +8,21 @@ function Home() {
 
     return (
       <>
-    
-        {/* //<img src={photo} alt="My Photo" /> */}
+     
+        < div className="home">
+        
         <h1>Isabel Solana</h1>
+        <img src={photo} alt="My Photo" /> 
         <p>
          Freelance frond end developer based in London. I specialise in building responsive, user-friendly websites and applications using HTML, CSS, JavaScript and React.
          Enrolled in a Frond-End development bootcamp with EdX.
         </p>
-        <h2> Welcome to my Portfolio page</h2>
+        <h3> Welcome to my Portfolio page!</h3>
+        <h4>Click below to have a look to my Project Gallery</h4>
         <Link to="/projects" role="button" className="btn btn-link">
-          Projects
+          Project Gallery
         </Link>
-       
+       </div>
       </>
     );
   }
